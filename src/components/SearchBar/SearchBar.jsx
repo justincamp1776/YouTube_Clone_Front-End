7728c0b493f06d656b0ea3c56b6a84086689baa1
justincamp1776 <justincamp1776@gmail.com>
2021-10-15 12:01:39 -0400

    Installed material ui + Added SearchBar component + added get request that returns 5 objects from youtube api

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import { Paper, TextField } from '@mui/material';
+import react, {Component} from 'react'
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            searchTerm : '',
+        }
+    }    
+
+    handleChange = (event) =>{
+        this.setState({
+            searchTerm : event.target.value
+        })
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        this.props.formSubmission(this.state)
+    }
+  
+    render() { 
+        return (  
+            <Paper elevation={6} style={{padding:'25px'}}>
+            <form onSubmit={this.handleSubmit}>
+                <TextField fullWidth label="Search for..." onChange={this.handleChange}></TextField>
+            </form>
+            </Paper>
+        );
+    }
+}
+ 
+export default SearchBar;
---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './VideoPlayer.css';
+
+const VideoPlayer = (props) => {
+    return (  
+        <h1>This is a video player</h1>
+    );
+}
+ 
+export default VideoPlayer;
