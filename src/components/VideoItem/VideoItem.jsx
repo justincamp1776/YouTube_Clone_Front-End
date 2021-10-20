1cf874b694f0c743e937722e67644361780071fe
justincamp1776 <justincamp1776@gmail.com>
2021-10-20 09:28:18 -0400

    Moved the jump to next to the title in related videos

--- src/components/VideoItem/VideoItem.jsx
+++ src/components/VideoItem/VideoItem.jsx
@@ -11,8 +11,8 @@ const VideoItem = (props)=>{
         <Grid item xs={12}>
             <Paper style={{display:'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => onVideoSelect(video)}>
             <img style={{maringRight: '20px'}} alt="thumbnail" src={video.snippet.thumbnails.medium.url}/>
-            <Typography variant="subtitle1"><b>{video.snippet.title}</b></Typography>
-            <a href="#videoPlayer">CLICK HERE</a>
+            <Typography variant="subtitle1"><b>{video.snippet.title}</b> <a href="#videoPlayer">CLICK HERE</a> </Typography>
+            
             </Paper>
         </Grid>
     );
